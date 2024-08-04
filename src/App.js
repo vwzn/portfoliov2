fc670f475dab03f443f308de933314229c89e43e
Fauzan <fauzan.abd.1703@gmail.com>
2024-08-04 09:06:54 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './components/Home'; // Ensure the correct path to the component
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import HomeBg from './image/home.jpg';
+import Me from './image/me.png';
+import PortfolioV1 from './image/portfoliov1.jpg'
+export { HomeBg, Me, PortfolioV1 };
